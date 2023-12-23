97f93cf0ae69138d7a8d880a88f6bcbe56000185
evanmulcare <107214132+evanmulcare@users.noreply.github.com>
2023-12-23 19:40:50 +0000

    Add files via upload

---
+++ Android/app/build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.healthai"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.healthai"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+
+    buildFeatures{
+        viewBinding  = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildToolsVersion = "34.0.0"
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:32.4.0"))
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.google.firebase:firebase-auth")
+    implementation ("com.google.firebase:firebase-firestore")
+    implementation ("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-database")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation ("com.squareup.picasso:picasso:2.71828")
+
+
+    implementation("com.android.volley:volley:1.2.0")
+    implementation ("com.paypal.sdk:paypal-android-sdk:2.16.0")
+
+
+
+    implementation ("com.paypal.sdk:paypal-android-sdk:2.16.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // RecyclerView
+    implementation ("androidx.recyclerview:recyclerview:1.3.1")
+
+    // CardView
+    implementation ("androidx.cardview:cardview:1.0.0")
+
+    // Glide
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+
+    // CircleImageView
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+}
+
+
