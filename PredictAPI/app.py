4751856d96e48686cf78fb540236fc8d74b22dd2
evanmulcare <107214132+evanmulcare@users.noreply.github.com>
2023-12-23 19:23:06 +0000

    Add files via upload

---
+++ PredictAPI/app.py
@@ -0,0 +1,144 @@
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
+import numpy as np
+import os
+from heart_disease import predict_heart_disease
+from lung_cancer import predict_lung_cancer
+from diabetes import predict_diabetes
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+    return "HealthAI Backend"
+
+@app.route('/predict-heart-disease', methods=['POST'])
+def predict_heart():
+    input_data = request.get_json()
+
+    expected_keys = [
+        'age', 'sex', 'ChestPainType', 'BP', 'Cholesterol', 'FBSOver120',
+        'EKGResults', 'MaxHR', 'ExerciseAngina', 'STdepression', 'SlopeOfST',
+        'NumberOfVesselsFluro', 'Thallium'
+    ]
+    
+    if not all(key in input_data for key in expected_keys):
+        error_message = 'Invalid input data'
+        print("Error:", error_message)
+        return jsonify({'error': error_message})
+
+    input_data_list = [int(input_data[key]) for key in expected_keys]
+
+    result = predict_heart_disease(input_data_list)
+
+    return jsonify({'result': result})
+
+@app.route('/predict-lung-disease', methods=['POST'])
+def predict_lung():
+    input_data = request.get_json()
+
+    expected_keys = [
+        'GENDER', 'AGE', 'SMOKING', 'YELLOW_FINGERS', 'ANXIETY', 'PEER_PRESSURE',
+        'CHRONIC_DISEASE', 'FATIGUE', 'ALLERGY', 'WHEEZING', 'ALCOHOL_CONSUMING',
+        'COUGHING', 'SHORTNESS_OF_BREATH', 'SWALLOWING_DIFFICULTY', 'CHEST_PAIN'
+    ]
+    
+    if not all(key in input_data for key in expected_keys):
+        error_message = 'Invalid input data'
+        print("Error:", error_message)
+        return jsonify({'error': error_message})
+    
+    input_data_list = [(input_data[key]) for key in expected_keys]
+
+    result = predict_lung_cancer(input_data_list)
+
+    return jsonify({'result': result})
+
+
+@app.route('/predict-diabetes', methods=['POST'])
+def predict_diab():
+    input_data = request.get_json()
+
+    expected_keys = [
+        'Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI',
+        'DiabetesPedigreeFunction', 'Age'
+    ]
+    
+    if not all(key in input_data for key in expected_keys):
+        error_message = 'Invalid input data'
+        print("Error:", error_message)
+        return jsonify({'error': error_message})
+    
+    input_data_list = [(input_data[key]) for key in expected_keys]
+
+    result = predict_diabetes(input_data_list)
+
+    return jsonify({'result': result})
+
+
+
+@app.route('/download-heart-data-csv', methods=['GET'])
+def download_heart_data_csv():
+    data_path = 'Data/Heart_Disease_Prediction.csv'
+    
+    # Check if the CSV file exists
+    if os.path.exists(data_path):
+        return send_file(data_path, as_attachment=True)
+    else:
+        return jsonify({'error': 'CSV file not found'})
+
+@app.route('/download-heart-data-accuracy-txt', methods=['GET'])
+def download_heart_data_accuracy_txt():
+    data_path = 'Data/heart_disease_accuracy.txt'
+    
+    # Check if the text file exists
+    if os.path.exists(data_path):
+        return send_file(data_path, as_attachment=True)
+    else:
+        return jsonify({'error': 'Text file not found'})
+
+
+
+@app.route('/download-lung-data-csv', methods=['GET'])
+def download_lung_data_csv():
+    data_path = 'Data/Lung_Cancer_Detection.csv'
+    
+    # Check if the CSV file exists
+    if os.path.exists(data_path):
+        return send_file(data_path, as_attachment=True)
+    else:
+        return jsonify({'error': 'CSV file not found'})
+    
+@app.route('/download-lung-data-accuracy-txt', methods=['GET'])
+def download_lung_data_accuracy_txt():
+    data_path = 'Data/lung_cancer_accuracy.txt'
+    
+    # Check if the text file exists
+    if os.path.exists(data_path):
+        return send_file(data_path, as_attachment=True)
+    else:
+        return jsonify({'error': 'Text file not found'})
+    
+@app.route('/download-diabetes-data-csv', methods=['GET'])
+def download_diabetes_data_csv():
+    data_path = 'Data/Diabetes_Detection.csv'
+    
+    # Check if the CSV file exists
+    if os.path.exists(data_path):
+        return send_file(data_path, as_attachment=True)
+    else:
+        return jsonify({'error': 'CSV file not found'})
+    
+@app.route('/download-diabetes-data-accuracy-txt', methods=['GET'])
+def download_diabetes_data_accuracy_txt():
+    data_path = 'Data/diabetes_accuracy.txt'
+    
+    # Check if the text file exists
+    if os.path.exists(data_path):
+        return send_file(data_path, as_attachment=True)
+    else:
+        return jsonify({'error': 'Text file not found'})
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ PredictAPI/heart_disease.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import joblib
+from sklearn.metrics import accuracy_score
+
+def train_heart_disease_model():
+    # Load the heart disease dataset
+    heart_data = pd.read_csv('Data/Heart_Disease_Prediction.csv')
+
+    # Define features (X) and target (Y)
+    X = heart_data.drop(columns='Heart Disease', axis=1)
+    Y = heart_data['Heart Disease']
+
+    # Split the data into training and test sets
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
+
+    # Train a logistic regression model
+    model = LogisticRegression()
+    model.fit(X_train, Y_train)
+    Y_pred = model.predict(X_test)
+
+    # Calculate accuracy
+    accuracy = accuracy_score(Y_test, Y_pred)
+    # Save the trained model to a file
+    joblib.dump(model, 'Models/heart_disease_model.joblib')
+
+    # Save accuracy to a file
+    with open('Data/heart_disease_accuracy.txt', 'w') as accuracy_file:
+        accuracy_file.write(f'Accuracy: {accuracy:.2f}')
+
+def predict_heart_disease(input_data):
+    # Check if the model file exists
+    try:
+        model = joblib.load('Models/heart_disease_model.joblib')
+    except FileNotFoundError:
+        # If the file doesn't exist, train a new model
+        train_heart_disease_model()
+        model = joblib.load('Models/heart_disease_model.joblib')
+
+    # Create a dictionary for input data
+    input_data_dict = {
+        'Age': [input_data[0]],
+        'Sex': [input_data[1]],
+        'Chest pain type': [input_data[2]],
+        'BP': [input_data[3]],
+        'Cholesterol': [input_data[4]],
+        'FBS over 120': [input_data[5]],
+        'EKG results': [input_data[6]],
+        'Max HR': [input_data[7]],
+        'Exercise angina': [input_data[8]],
+        'ST depression': [input_data[9]],
+        'Slope of ST': [input_data[10]],
+        'Number of vessels fluro': [input_data[11]],
+        'Thallium': [input_data[12]]
+    }
+
+    # Create a DataFrame with the same columns as during training
+    input_data_df = pd.DataFrame(input_data_dict)
+
+    # Make a prediction using the trained model
+    prediction = model.predict(input_data_df)
+
+    # Return 1 if the prediction is 1 (Presence), else return 0 (Absence)
+    return prediction[0]
